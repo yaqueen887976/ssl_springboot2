5da7a198571f7adfb28f61f3ba4eb7d42eeb415c
yaqueen887976 <yaqinwang@email.arizona.edu>
2020-04-19 23:09:33 -0600

    modify3

--- src/main/java/com/yaqin/spring_boot_hw2/controller/DocumentController.java
+++ src/main/java/com/yaqin/spring_boot_hw2/controller/DocumentController.java
@@ -4,11 +4,14 @@ import com.yaqin.spring_boot_hw2.entity.Document;
 import com.yaqin.spring_boot_hw2.exception.DocumentNotFoundException;
 import com.yaqin.spring_boot_hw2.repository.DocumentRepository;
 import com.yaqin.spring_boot_hw2.response.ResponseCreate;
+import com.yaqin.spring_boot_hw2.response.ResponseGet;
+import com.yaqin.spring_boot_hw2.service.DocumentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,34 +19,39 @@ import java.util.List;
 public class DocumentController {
 
     private final DocumentRepository documentRepository;
-
+    private DocumentService documentService;
 
     DocumentController(DocumentRepository documentRepository){
         this.documentRepository = documentRepository;
     }
 
-
     @PostMapping
-    //@PostMapping("/storage/documents")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ResponseCreate<String>>  newDocument(@RequestBody Document newDocument) throws InterruptedException{
         documentRepository.save(newDocument);
-
-        return new ResponseEntity<>(new ResponseCreate<String>(HttpStatus.valueOf("CREATED").toString(), "text/plain;", newDocument.getContent()), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseCreate<String>(HttpStatus.valueOf("CREATED").toString(),
+                "text/plain;",
+                newDocument.getContent()),
+                HttpStatus.CREATED);
     }
 
     // Single item
 
     @GetMapping("/storage/documents/{docId}")
-    Document one(@PathVariable Long id) {
-        return documentRepository.findById(id)
+    ResponseEntity<ResponseGet> one(@PathVariable Long id) {
+        documentRepository.findById(id)
                 .orElseThrow(() -> new DocumentNotFoundException("Application Not Found"));
+
+        return new ResponseEntity<>(new ResponseGet(HttpStatus.valueOf("OK").toString(),
+                documentService.findDocument(id).getContent().length(),
+                documentService.findDocument(id).getContent()),
+                HttpStatus.CREATED);
     }
 
     @PutMapping("/storage/documents/{docId}")
-    Document replaceDocument(@RequestBody Document newDocument, @PathVariable Long id) {
+    String replaceDocument(@RequestBody Document newDocument, @PathVariable Long id) {
 
-        return documentRepository.findById(id)
+        documentRepository.findById(id)
                 .map(document -> {
                     document.setContent(newDocument.getContent());
                     return documentRepository.save(document);
@@ -52,10 +60,13 @@ public class DocumentController {
                     newDocument.setId(id);
                     return documentRepository.save(newDocument);
                 });
+
+        return "204 No Content";
     }
 
     @DeleteMapping("/storage/documents/{docId}")
-    void deleteDocument(@PathVariable Long id) {
+    String deleteDocument(@PathVariable Long id) {
         documentRepository.deleteById(id);
+        return "204 No Content";
     }
 }
--- src/main/java/com/yaqin/spring_boot_hw2/response/ResponseGet.java
+++ src/main/java/com/yaqin/spring_boot_hw2/response/ResponseGet.java
@@ -8,20 +8,46 @@ hello world*/
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+/*Response:
+200 OK
+Content-Length: 11
+hello world*/
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class ResponseGet<T> {
+public class ResponseGet {
 
-    private Integer code;
+    private String code;
 
-    private String message;
+    private int content_length;
 
-    private T content;
+    private String content;
 
-    public ResponseGet(Integer code, String message, T content) {
+    public ResponseGet(String code, int content_length, String content) {
         this.code = code;
-        this.message = message;
+        this.content_length = content_length;
         this.content = content;
     }
 
+    public String getCode() {
+        return code;
+    }
 
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getContent_length() {
+        return content_length;
+    }
+
+    public void setContent_length(int content_length) {
+        this.content_length = content_length;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
